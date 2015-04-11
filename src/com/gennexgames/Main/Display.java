d08438b3b7cf2ff2ec5b8f71d298a61185fe4096
danydo11 <d.shedlock1@hotmail.com>
2015-04-11 22:27:34 +0100

    Changed Box color
    clean up Display class too

--- src/com/gennexgames/Main/Display.java
+++ src/com/gennexgames/Main/Display.java
@@ -3,14 +3,17 @@ package com.gennexgames.Main;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-
 import javax.swing.JPanel;
+
 
 public class Display extends JPanel {
 
+	
 	public int n = 0;
 	
+	
 	Frame frame;
+	
 	
 	public Display(Frame frame){
 		this.frame = frame;
@@ -19,26 +22,37 @@ public class Display extends JPanel {
 	
 	@Override
 	public void paintComponent(Graphics g){
+		
+		
 		int boxWidth = (int) (frame.getWidth() * (9d/14d));
 		int boxHeight = (int) (frame.getHeight() * (9d/14d));
 		
+		
+		Color viewScreenColor = new Color(0, 148, 255);
+		Color codeScreenColor = new Color(0, 255, 33);
+		Color commandScreenColor = new Color(255, 106, 0);
+		
+		
 		g.clearRect(0,0,frame.getWidth(),frame.getHeight());
 		
-		g.setColor(Color.GREEN);
+		
+		//g.setColor(Color.GREEN);
+		g.setColor(codeScreenColor);
 		g.fillRect(0, 0, frame.getWidth(), frame.getHeight());
 		
 		
-		g.setColor(Color.CYAN);
-		g.fillRect(0, 0, (int)boxWidth , (int)boxHeight);
+		//g.setColor(Color.CYAN);
+		g.setColor(viewScreenColor);
+		g.fillRect(0, 0, boxWidth , boxHeight);
 		
 		
-		g.setColor(Color.ORANGE);
-		g.fillRect(0, (int)boxHeight, (int)boxWidth , (frame.getHeight()) - (int)boxHeight);
+		//g.setColor(Color.ORANGE);
+		g.setColor(commandScreenColor);
+		g.fillRect(0, boxHeight, boxWidth , (frame.getHeight()) - boxHeight);
 		
 		
 		g.setColor(Color.BLACK);
 		g.setFont(new Font("TimesRoman",Font.PLAIN,100));
 		g.drawString(n + "", 0, 500);
 	}
-	
 }
